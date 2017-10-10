193acf02959e5ec8bd6dd1eb9d5277e0ab8d17a4
SamHemming <samhem@metropolia.fi>
2017-10-11 00:01:16 +0300

    Added Inventory and renamed resources folder :I
    all the changes :D

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -10,16 +10,25 @@ public class Inventory : MonoBehaviour {
 	private Transform itemPanel;
 	private List<InteractableObject> inventoryList;
 	private GameObject itemSlotPrefab;
+	private List<GameObject> itemSlotList;
+	private Button sortButton;
+	private Text sortButtonText;
+	private string sortType = "A-Z";
 
 	//------------------------------------------------------------------------
 
 	void Start () {
 		gameObject.GetComponent<Button> ().onClick.AddListener(()=>InventoryToggle());
 		inventoryPanel = GameObject.Find ("InventoryPanel");
-		inventoryPanel.SetActive(false);
-		inventoryList = GameObject.FindObjectOfType<GameController> ().player.inventory;
-		itemSlotPrefab = (GameObject)Resources.Load("ItemSlot.prefab");
 		itemPanel = GameObject.Find ("ItemPanel").GetComponent<Transform>();
+		inventoryList = GameObject.FindObjectOfType<GameController> ().player.inventory;
+		itemSlotPrefab = (GameObject)Resources.Load("ItemSlot");
+		itemSlotList = new List<GameObject>();
+		sortButton = GameObject.Find ("InventorySortButton").GetComponent<Button> ();
+		sortButton.onClick.AddListener (() => SortInventory ());
+		sortButtonText = sortButton.GetComponentInChildren<Text> ();
+
+		inventoryPanel.SetActive(false);
 	}
 
 	//------------------------------------------------------------------------
@@ -38,13 +47,46 @@ public class Inventory : MonoBehaviour {
 	//------------------------------------------------------------------------
 
 	private void InventoryUpdate() {
-		foreach (InteractableObject item in inventoryList) {
-			GameObject itemSlot = Instantiate(itemSlotPrefab, itemPanel);
+		foreach (GameObject item in itemSlotList) {	//remove the old itemSlot objects
+			Destroy (item);
+		}
+		itemSlotList.Clear ();	//clear the list
 
+		foreach (InteractableObject item in inventoryList) {	//make new itemSlot for every item in the inventory
+			GameObject itemSlot = Instantiate(itemSlotPrefab, itemPanel);	//make new itemSlot from prefab
+			itemSlotList.Add (itemSlot);	//add item to list
+			itemSlot.GetComponent<ItemSlot>().Item = item;	//set itemSlot gameObjects property Item item from the inventory
 		}
 	}
 
 	//------------------------------------------------------------------------
+
+	private void SortInventory() {
+		switch (sortType) {
+
+		case "A-Z":
+			//inventoryList.Sort ();
+			sortType = "Z-A";
+			break;
+
+		case"Z-A":
+			sortType = "type";
+			break;
+
+		case "type":
+			sortType = "!type";
+			break;
+
+		case "!type":
+			sortType = "A-Z";
+			break;
+
+		default:
+			Debug.Log ("SortInventory default case.");
+			break;
+		}
+		sortButtonText.text = sortType;	//Update sort buttons text
+	}
 
 	// Update is called once per frame
 	void Update () {
--- Assets/Scripts/ItemSlot.cs
+++ Assets/Scripts/ItemSlot.cs
@@ -1,18 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
-public class ItemSlot {
+public class ItemSlot : MonoBehaviour {
 
-	private InteractableObject item;
+	public InteractableObject Item { get; set;}	//item property of interactableItem type
+	public static ItemSlot SelectedSlot { get; set; }
 
-	// Use this for initialization
+	public delegate void SlotChangeDelegate();	//make template for referensing methods
+	public event SlotChangeDelegate SlotChangeEvent;	//when SelectedSlotChanged is called, all methods that fit into SlotChangeEvent are called
+
+	private Button myButton;
+	private Inventory invScript;
+	private Text descriptionText;
+	private Text headerText;
+
+	//----------------------------------------------------------------------------------------
+
 	void Start () {
-		
+		myButton = this.GetComponent<Button> ();
+		myButton.onClick.AddListener (() => Select ());
+		invScript = GameObject.FindObjectOfType<Inventory> ();
+		SlotChangeEvent += new SlotChangeDelegate(SlotChangeHandler);
+		descriptionText = GameObject.Find ("DescriptionText").GetComponent<Text> ();
+		headerText = GameObject.Find ("DescriptionHeader").GetComponent<Text> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	//----------------------------------------------------------------------------------------
+
+	private void Select() {
+		//Debug.Log ("Select fired from: " + this.GetInstanceID());
+		SelectedSlot = this;
+		SlotChangeEvent ();
+	}
+
+	//----------------------------------------------------------------------------------------
+
+	void SlotChangeHandler() {
+		if (SelectedSlot == this) {
+			myButton.GetComponent<Image> ().color = Color.red;
+			Debug.Log ("SlotChangeHandler fired in if: " + this.GetInstanceID());
+			descriptionText.text = Item.description;
+			headerText.text = Item.name;
+		}
+		else {
+			myButton.GetComponent<Image> ().color = Color.white;
+			Debug.Log ("SlotChangeHandler fired in else: " + this.GetInstanceID());
+		}
 	}
 }
